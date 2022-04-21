96d8ccffdbaaefad580b25a44d6bfa724f506ae8
Jonathan Cagua <jonathan.cagua@gmail.com>
2022-04-20 21:32:59 -0500

    first commit

---
+++ inc/asm_func.h
@@ -0,0 +1,6 @@
+#include <stdint.h>
+
+
+extern uint32_t asm_sum (uint32_t firstOperand, uint32_t secondOperand);
+extern void asm_svc (void);
+extern void asm_zero (uint32_t *vector,uint32_t longitud);
---
+++ src/main.c
@@ -0,0 +1,29 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32l4xx.h"
+#include "stm32l475e_iot01.h"
+			
+
+
+int main(void)
+{
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+//  uint8_t testing = 0;
+  uint32_t aValue = 20,otherValue = 30,sumResult;
+  while (1)
+  {
+	  sumResult = asm_sum(aValue, otherValue);
+  }
+
+}
+
